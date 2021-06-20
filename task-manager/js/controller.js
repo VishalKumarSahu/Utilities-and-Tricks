aa651f0cd5bbe23dd2616e8fabf3275380322ce4
Vishal K Sahu <hello@vishalkumarsahu.in>
2021-06-21 02:01:18 +0530

    Removed data.js

--- task-manager/js/controller.js
+++ task-manager/js/controller.js
@@ -1,7 +1,44 @@
+/*
+Create a simple task manager using html css and javascript.This task manager has 2 vertical sections:
+- Todo: List of tasks to be done
+- Done: List of tasks which have already been done.
+
+Each task will have a name and a description. 
+There should be an empty form on the top of the 'Todo' list. 
+A user can create a task in the 'Todo' list  by filling up that form. 
+Once the task is done, he/she can move that task to the 'Done' section. 
+You can either implement this using a button called 'Done' on every task on the 'Todo' list 
+or by dragging and dropping the task from one list to another. 
+The user should be able to see his/her lists whenever he/she opens the app in the browser. 
+For this you will need to save the list in localStorage every time a change has been made in the list.https://www.w3schools.com/jsref/prop_win_localstorage.asp
+
+
+You are free to use any css framework or not to use it. However you can only use pure Javascript.
+
+*/
+
+// One has to load the tasks from localStorage
+// Copy and paste the following in your console to see how you can store it in localStorage:
+// - localStorage.setItem("tm_list", JSON.stringify([{ name: "Create HTML and CSS", description: "Simple layout wth HTML and CSS" }]))
+// - localStorage.getItem("tm_list")
+// - JSON.parse(localStorage.getItem("tm_list"))
+
+// new Date().valueOf()
+
+// Alternative choice of list structure 
+let tasks = [
+	// {
+	// 	title: "Task Title",
+	// 	description: "Task Description",
+	// 	date_added: 1623951099481,
+	// 	date_last_updated: 1623951099481,
+	// 	status: 0
+	// }
+];
+
 // Populates list
 // Creates DOM for the lists
 // Could be used every time the lists are updated
-tasks = tasks || [];
 
 function populateLists(){
 	// Loop through list or lists of tasks 
@@ -35,7 +72,7 @@ function populateLists(){
 			let el_task_button = document.createElement("button");
 			let el_task_datetime = document.createElement("span");
 	
-			let date = new Date(task.status ? task.date_last_updated : task.date_added);
+			let date = new Date(task.status ? date_last_updated : date_added);
 
 			let datetime = (date.getDate()+
           "/"+(date.getMonth()+1)+
